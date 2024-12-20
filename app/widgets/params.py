5e0f6cec16a4257a2d9f5111985724f35e05a202
Benjamin DUBUS <benjamin.dubus@outlook.com>
2024-12-20 18:13:54 +0100

    refactor: remove PlotData model and update author information across multiple files

--- app/widgets/params.py
+++ app/widgets/params.py
@@ -1,3 +1,18 @@
+"""PELCA (Power Electronics Life Cycle Assessment) is an open-source project aimed at assessing the environmental impact over the life cycle of modular and diagnosable power electronics systems. The integration of modularity and diagnosability aligns with circular economy principles, promoting practices such as repair and reuse. This project provides a tool to calculate the environmental impacts associated with the manufacturing, usage, and replacement of power electronics products.
+Copyright (C) Mitsubishi Electric R&D Centre Europe and SATIE 2024, author Briac Baudais baudaisbriac@gmail.com
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this program.  If not, see https://www.gnu.org/licenses/lgpl-3.0.html"""
+
+"""
+Created on 2024
+
+@author: baudais, bdubus
+"""
+
 import configparser
 
 from PySide6.QtWidgets import (QApplication, QCheckBox, QDoubleSpinBox,
